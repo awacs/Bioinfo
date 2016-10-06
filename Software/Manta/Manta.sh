4139a8268ffa77785903848b47dd9e5d54a694fb
awacs <haroldzwang@gmail.com>
2016-10-06 13:42:31 -0400

    Manta masterscript init

--- Software/Manta/Manta.sh
+++ Software/Manta/Manta.sh
@@ -1,3 +1,51 @@
 #!/bin/bash
+set -e
 # This code is to run the manta workflow
+# This code assumes the manta installation path is known
 ##################
+help_message()
+{
+echo -e "./Manta.sh -b[bam,requried] -r [.fa reference, required] -o [outputdirectory,optional,default="."]"
+}
+module use /apps/modulefiles/lab/miket
+module load manta/0.29.5
+# Default Variables
+OUT_DIR="."
+# Argument parsing
+if [ $# -eq 0 ];
+then
+    help_message
+    exit 1
+else
+    while getopts ":he:r:o:" opt; do
+        case $opt in
+            h ) help_message
+                exit 0 ;;
+            e ) echo "bams = $OPTARG "
+                set -f # disable glob
+                IFS=',' # split on space characters
+                BAMFILE=$OPTARG ;; # use the split+glob operator
+            r ) echo "REFERENCE = $OPTARG "
+                FLAG=1
+                set -f # disable glob
+                IFS=',' # split on space characters
+                REFFA=$OPTARG ;; # use the split+glob operator
+            o ) echo "output directory = $OPTARG" 
+                OUT_DIR=$OPTARG 
+                mkdir -p $OUT_DIR ;; 
+            * ) help_message
+                exit 1 ;;
+        esac
+    done
+fi
+# CONFIGURATION STEP
+# Single Sample
+BAMFILE=~/Rawdata/practice_bams/14415.mo.bam
+REFFA=~/Rawdata/manta_practice.fa
+OUT_DIR=~/Rawdata/practice_bams/Manta
+configManta.py \
+    --bam $BAMFILE \
+    --referenceFasta $REFFA \
+    --runDir $OUT_DIR
+# Running Step
+$OUT_DIR/runWorkflow.py -m sge 
